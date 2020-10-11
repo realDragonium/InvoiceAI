5319bb3e66aa8fe226c96af21a1d79a0a2b267be
realDragonium <drago@dragonium.nl>
2020-10-11 20:34:17 +0200

    switched to vue2 for css framework. Basic design page on /login path added default cors settings to fiber

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"invoiceai/database"
 	"invoiceai/handler"
 	"invoiceai/login"
@@ -10,8 +12,13 @@ import (
 
 func main() {
 	app := fiber.New()
+	// Default config
+	app.Use(cors.New())
 	database.InitDatabase()
 	login.Migrate()
 	handler.SetupRoutes(app)
-	app.Listen(":3000")
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
